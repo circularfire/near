223284c752e4943131cb7824692c241a84761962
tom biggs <tbiggs@comcast.net>
2018-03-04 14:23:31 -0500

    WIP: scanning files for matches successfully

--- near.py
+++ near.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 import sys
-
+import re
 
 class Term(object):
     def __init__(self, term_str):
         self.term = term_str
-        self.matcher = None # re.compile(^^)
+        self.matcher = re.compile(self.term)
 
 class AllTerms(list):
     def add(self, term_str):
@@ -15,6 +16,12 @@ class FileTerm(object):
         self.term = term  # Term object
         self.lines = []
 
+    def match(self, lineno, line):
+        if re.search(self.term.matcher, line):
+            self.lines.append(lineno)
+
+    def __str__(self):
+        return "/{}/: {}".format(self.term.term, repr(self.lines))
 
 class Window(object):
     def __init__(self):
@@ -30,15 +37,20 @@ class SearchFile(object):
         for term in terms:
             self.terms.append(FileTerm(term))
 
-
     def process(self):
-        with open(self.filename) as f:
-            self.contents = f.read.splitlines()
+        with open(self.name) as f:
+            self.contents = list(f)
+        for lineno, line in enumerate(self.contents):
+            for term in self.terms:
+                term.match(lineno, line)
+
+        for term in self.terms:
+            print(str(term))
 
 
 class AllSearchFiles(list):
-    def add(self, filename):
-        self.append(SearchFile(filename))
+    def add(self, filename, terms):
+        self.append(SearchFile(filename, terms))
 
 
 class Config(object):
@@ -51,7 +63,7 @@ CONFIG = Config()
 
 def go():
     if len(sys.argv) < 4:
-        printf("usage term1 term2 file [file...]")
+        print("usage term1 term2 file [file...]")
         sys.exit(1)
 
     CONFIG.terms.add(sys.argv[1])
@@ -59,5 +71,10 @@ def go():
     for filename in sys.argv[3:]:
         CONFIG.files.add(filename, CONFIG.terms)
 
+    for file in CONFIG.files:
+        file.process()
 
 
+
+if __name__ == '__main__':
+    go()
