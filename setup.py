30f1385fa8bafe75eb5dec27d9d73a0e1836cf8e
tom biggs <tbiggs@comcast.net>
2018-03-09 22:29:43 -0500

    fixed setup.py to allow installation as a utility

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="near",
-    version='0.1',
+    name='near',
+    version='0.2',
     py_modules=['near'],
     install_requires=[
         'Click',
     ],
-    entry_points = {
-        'console_scripts': ['near=near:go'],
-    }
+    entry_points='''
+        [console_scripts]
+        near=near:cli
+    ''',
 )
